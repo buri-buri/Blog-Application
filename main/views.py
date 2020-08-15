2bb9ff6152835ecc77e6fc76e1ca957a4e2f7203
buriburi <sagarshayr999@gmail.com>
2020-08-15 16:58:21 +0530

    yahi mai top 5 articles print kra rha hu, let mee blog app mai 100 articles hai to front-end pe mai bss 5 latest article print kraauga, logic views.py mai hai dekh liyo, pehle maine latest 5 articles uthaaye phir uss list ko dict mai daala and then  usko index.html mai paas kr diya, now uss list ko mai using loop print kra rha hu, dekh liyo index.html mai

--- main/views.py
+++ main/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main import models
 # Create your views here.
 def index(request):
-    d={}
-    return render(request,'main/index.html')
+    n=len(models.Article.objects.all())
+    latest_article=models.Article.objects.all()
+    if(n>=5):
+        latest_article=latest_article[n-5::][::-1]
+    else:
+        latest_article=latest_article[::-1]
+    '''
+    hum isko SQL se bhi kr skte the by using ORDER_BY clause
+    latest_Article=models.Article.objects.all().order_by('-createdAT')[:5]
+    yha minus sign isley lgaaya hai taaki descending order m aa jaaye.
+    '''
+    d={
+        'length':n,
+        'latest_article':latest_article
+    }
+    return render(request,'main/index.html',d)
